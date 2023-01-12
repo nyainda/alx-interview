9a790ba775d909749f237ae56e0eba5b5d3053f3
* <oyugibruce2017@gmail.com>
2023-01-12 21:57:22 +0300

    minimum

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
+"""
+Minimum Operations
+Given num n, write a method that calculates the fewest number of operations
+needed to result in exactly n H characters in a file
+Prototype: def minOperations(n)
+Returns an integer
+if n is impossible to achieve, return 0
+"""
 
-from math import sqrt, ceil
 
-'''This module defines a function that given a number n, minOperations(n) calculates the fewest number of operations needed to result in exactly n H characters in the file.
-    Returns an integer. If n is impossible to achieve, it returns 0.
-    '''
 def minOperations(n):
-    '''Given a number n, minOperations(n) calculates the fewest number of operations needed to result in exactly n H characters in the file.
-    Returns an integer. If n is impossible to achieve, it returns 0.
-    '''
-    if n <= 0:
-        return 0
-    num = sqrt(n)
-    ans =+ num * 2
-    if type(num) is float:
-        return round(ans)
-    else:
-        minOperations(num)
+    """
+    Function minOperations
+    Returns an integer
+    """
+    result = 0
+    x = 2
+    while n > 1:
+        while n % x == 0:
+            result += x
+            n /= x
+        x += 1
+    return result
