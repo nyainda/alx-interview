ebdd2e288564fb32e464b097364876bf39396480
* <oyugibruce2017@gmail.com>
2023-02-02 21:50:53 +0300

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,44 +1,69 @@
 #!/usr/bin/python3
 """
-The N Queens Problem
-Show all possible ways to place N non-attacking Queens on an NxN board
+Task 0: N queens
 """
 
 import sys
-import math
 
-solutions = []
-can_place_count = [0]
-nqueens_count = [0]
 
-def can_place(board, row, col):
-    """Checks if [row, col] is a safe position on the board"""
-    can_place_count[0] += 1
-    for i in range(row):
-        if board[i] == col or abs(i - row) == abs(board[i] - col):
-            return False
+def canMove(final, row, column):
+    """
+    Checks if the queen would be able to move
+    """
+    rows = []
+    columns = []
+    top_left = []
+    top_right = []
+
+    for nums in final:
+        rows.append(nums[0])
+        columns.append(nums[1])
+        top_left.append(nums[1] - nums[0])
+        top_right.append(nums[0] + nums[1])
+
+    if row in rows or column in columns:
+        return False
+    if column - row in top_left or row + column in top_right:
+        return False
+
     return True
 
 
-def nqueens(board, row, n):
-    """O(n!) time, O(n) space"""
-    nqueens_count[0] += 1
-    if row == n:
-        return solutions.append(board[:])
-    for col in range(n):
-        if can_place(board, row, col):
-            board[row] = col
-            nqueens(board, row + 1, n)
+def nqueens(final, column, checked_queens=[]):
+    """
+    The main recursive program
+    """
+    for item in range(n):
+        if canMove(final, item, column):
+            final.append([item, column])
+            if column == n - 1:
+                checked_queens.append(final.copy())
+                del final[-1]
+            else:
+                nqueens(final, column + 1)
 
-if __name__ == "__main__":
+    if len(final) > 0:
+        del final[-1]
+    return checked_queens
+
+if __name__ == '__main__':
+
     if len(sys.argv) != 2:
-        print("Usage: nqueens N") or exit(1)
+        print("Usage: nqueens N")
+        sys.exit(1)
+
     try:
         n = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number") or exit(1)
+    except:
+        print("N must be a number")
+        sys.exit(1)
+
     if n < 4:
-        print("N must be at least 4") or exit(1)
-    nqueens([None] * n, 0, n)
-    [print([[row, col] for row, col in enumerate(sol)]) for sol in solutions]
-    print(n, can_place_count, nqueens_count, can_place_count[0]/nqueens_count[0], math.factorial(n), can_place_count[0]/(pow(2, n) + n))
+        print("N must be at least 4")
+        sys.exit(1)
+
+    final = []
+    final = nqueens(final, 0)
+
+    for nums in final:
+        print(nums)
