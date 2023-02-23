02425740376dff176aab7e2afb4f5c903b9e6b34
* <oyugibruce2017@gmail.com>
2023-02-23 16:19:41 +0300

    making_change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Making change O(n)"""
+'''Making Change module'''
 
 
 def makeChange(coins, total):
-    """Clasic Bottom-Up dynamic programming"""
-    if total <= 0:
+    '''Function that makes change for a given total'''
+    if total < 1:
         return 0
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-    coins.sort()
-    for i in range(total + 1):
-        for coin in coins:
-            if coin > i:
-                break
-            if dp[i - coin] != -1:
-                dp[i] = min(dp[i - coin] + 1, dp[i])
-    return dp[total] if dp[total] != float('inf') else -1
+    change = 0
+    coins.sort(reverse=True)
+    for coin in coins:
+        temp_change = int(total / coin)
+        total -= (temp_change * coin)
+        change += temp_change
+        if total == 0:
+            return change
+    if total != 0:
+        return -1
